214fc4b97d6d7c72d812993018175ec1a753f6b0
Spider Man <tranvandathtml0503@gmail.com>
2023-06-20 11:27:52 +0700

    setup

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.css";
+import { total } from "./utils";
+
+const reviews = [
+  {
+    name: "Evondev",
+    image: "",
+    stars: 5,
+    premiumUser: true,
+    date: "05/09/2022",
+  },
+  {
+    name: "CharkaUI",
+    image: "",
+    stars: 4,
+    premiumUser: false,
+    date: "03/08/2022",
+  },
+  {
+    name: "React Query",
+    image: "",
+    stars: 3,
+    premiumUser: false,
+    date: "04/08/2022",
+  },
+];
+
+function App() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <div className="review">
+        <div className="review-image">
+          <img src="https://source.unsplash.com/random" alt="" />
+        </div>
+        <div className="review-info">
+          Review total <strong>3</strong> | Last reviewed by{" "}
+          <strong>Evondev</strong> ⭐️
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export function total(a: number, b: number) {
+  return a + b;
+}
+// const abc = 12;
+const age: number = 10;
+console.log(total(21, 12));
